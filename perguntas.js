527b6922dda7b6a4f3a1fcb1694f1a910cb69c0d
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-10-17 08:21:51 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'One Piece',
+    'Qual é o nome do navio principal dos Piratas do Chapéu de Palha?',
+    'Thousand Sunny'
+)
+
+criaCartao(
+    'One Piece',
+    'Quem é o capitão dos Piratas do Chapéu de Palha?',
+    'Monkey D. Luffy'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual é o nome da fruta que Luffy comeu?',
+    'Gomu Gomu no Mi (Fruta da Borracha)'
+)
+
+criaCartao(
+    'One Piece',
+    'Quem é o espadachim da tripulação do Chapéu de Palha?',
+    'Roronoa Zoro'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual é o objetivo de Luffy?',
+    'Se tornar o Rei dos Piratas'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual o nome do primeiro navio dos Chapéu de Palha?',
+    'Going Merry'
+)
